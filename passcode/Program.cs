f8b937b96db00baf1e7da62b863f7ce39d3ee62c
Harkishan Grewal <harkishangrewal@csu.fullerton.edu>
2018-07-24 10:20:37 -0700

    Add passcode program

---
+++ passcode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace passcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the passcode: ");
+            var passcode = Console.ReadLine();
+
+            if(passcode == "secret")
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else
+            {
+                Console.WriteLine("The entered passcode is incorrect.");
+            }
+        }
+    }
+}
---
+++ passcode/README.md
@@ -0,0 +1,7 @@
+# Passcode
+This simple C# program prompts the user to enter a passcode. If the entered
+passcode matches the correct passcode, "secret," an authentication message is
+printed. Otherwise, the user is informed that the enter pass code is incorrect.
+
+## Purpose
+The purpose of this program is to implement conditional statements in C#.
