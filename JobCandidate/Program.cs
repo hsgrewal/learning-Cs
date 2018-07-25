7f1c672a53db990f3a1bd3ab0b3017f0013cf133
Harkishan Grewal <harkishangrewal@csu.fullerton.edu>
2018-07-24 20:55:24 -0700

    Replace if/else with switch

--- JobCandidate/Program.cs
+++ JobCandidate/Program.cs
@@ -10,25 +10,23 @@ namespace JobCandidate
 
             var years = int.Parse(Console.ReadLine());
 
-            if (years == 0)
+            switch (years)
             {
-                Console.WriteLine("Inexperienced");
-            }
-            else if (years == 1)
-            {
-                Console.WriteLine("Junior");
-            }
-            else if (years == 2)
-            {
-                Console.WriteLine("Intermediate");
-            }
-            else if (years == 3)
-            {
-                Console.WriteLine("Advanced");
-            }
-            else
-            {
-                Console.WriteLine("Senior");
+                case 0:
+                    Console.WriteLine("Inexperienced");
+                    break;
+                case 1:
+                    Console.WriteLine("Junior");
+                    break;
+                case 2:
+                    Console.WriteLine("Intermediate");
+                    break;
+                case 3:
+                    Console.WriteLine("Advanced");
+                    break;
+                default:
+                    Console.WriteLine("Senior");
+                    break;
             }
         }
     }
