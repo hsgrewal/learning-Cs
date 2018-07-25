764344169e0a4a936157edd994e02ddf4c212e8d
Harkishan Grewal <harkishangrewal@csu.fullerton.edu>
2018-07-24 18:50:42 -0700

    Add SchoolManager project

---
+++ SchoolManager/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SchoolManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentGrades = new int[10] { 50, 20, 30, 65, 80, 95, 97, 87, 43, 67};
+
+            Console.WriteLine(studentGrades[4]);
+        }
+    }
+}
---
+++ SchoolManager/README.md
@@ -0,0 +1,4 @@
+# School Manager
+
+A C# program designed to manage various details about a school's students
+and staff.
