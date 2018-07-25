f507bbcf8e2b92608b005e284114b65421e08ea3
Harkishan Grewal <harkishangrewal@csu.fullerton.edu>
2018-07-24 18:56:17 -0700

    Add foreach loop to display all grades

--- SchoolManager/Program.cs
+++ SchoolManager/Program.cs
@@ -6,9 +6,12 @@ namespace SchoolManager
     {
         static void Main(string[] args)
         {
-            var studentGrades = new int[10] { 50, 20, 30, 65, 80, 95, 97, 87, 43, 67};
+            var studentGrades = new int[10] { 50, 20, 30, 65, 80, 95, 97, 87, 43, 67 };
 
-            Console.WriteLine(studentGrades[4]);
+            foreach (var studentGrade in studentGrades)
+            {
+                Console.WriteLine(studentGrade);
+            }
         }
     }
 }
