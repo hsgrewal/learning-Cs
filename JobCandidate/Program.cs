19b7ebcd183767b535beae4ea3c110679fd032b7
Harkishan Grewal <harkishangrewal@csu.fullerton.edu>
2018-07-24 20:48:53 -0700

    Add JobCandidate project

---
+++ JobCandidate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace JobCandidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years of experience do you have?");
+
+            var years = int.Parse(Console.ReadLine());
+
+            if (years == 0)
+            {
+                Console.WriteLine("Inexperienced");
+            }
+            else if (years == 1)
+            {
+                Console.WriteLine("Junior");
+            }
+            else if (years == 2)
+            {
+                Console.WriteLine("Intermediate");
+            }
+            else if (years == 3)
+            {
+                Console.WriteLine("Advanced");
+            }
+            else
+            {
+                Console.WriteLine("Senior");
+            }
+        }
+    }
+}
---
+++ JobCandidate/README.md
@@ -0,0 +1,12 @@
+# Job Candidate
+
+A C# program that asks a job candidate for their years of experience. Then, it
+categorizes the candidate into the following experience levels:
+
+| Years of Experience | Experience Level |
+|:-------------------:|:----------------:|
+|         0           | Inexperienced    |
+|         1           | Junior           |
+|         2           | Intermediate     |
+|         3           | Advanced         |
+|         4+          | Senior           |
