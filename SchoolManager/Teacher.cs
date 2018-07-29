f9d83995738475e4dd9ef6f64df6eead9674b9c2
Harkishan Grewal <harkishangrewal@gmail.com>
2018-07-29 11:13:21 -0700

    Change from fields to autoimplemented properties

--- SchoolManager/Teacher.cs
+++ SchoolManager/Teacher.cs
@@ -6,7 +6,7 @@ namespace SchoolManager
 {
     class Teacher : Member, IPayee
     {
-                public string Subject;
+        public string Subject { get; set; }
 
         public void Pay()
         {
